f20ccfab2a781ae4c70b8e2c6bf77efb542ee687
Leo <leo@eu.com>
2024-10-01 22:31:12 +0300

    fix ripemd-160 algorithm

--- ripemd160_cuda.cu
+++ ripemd160_cuda.cu
@@ -1,182 +1,353 @@
-#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 #include <stdint.h>
-#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define RIPEMD160_BLOCK_SIZE 64  // 512 bits
-#define RIPEMD160_DIGEST_SIZE 20  // 160 bits
+#define RIPEMD160_DIGEST_SIZE 20
 
-// RIPEMD-160 constants (based on the specification)
-__device__ const uint32_t K[5] = { 0x00000000, 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xa953fd4e };
-__device__ const uint32_t Kp[5] = { 0x50a28be6, 0x5c4dd124, 0x6d703ef3, 0x7a6d76e9, 0x00000000 };
+#define ROL(x, n) (((x) << (n)) | ((x) >> (32 - (n))))
 
-// Left rotation operation
-__device__ __inline__ uint32_t rol(uint32_t x, uint32_t n) {
-    return (x << n) | (x >> (32 - n));
+#define F(x, y, z) ((x) ^ (y) ^ (z))
+#define G(x, y, z) (((x) & (y)) | (~(x) & (z)))
+#define H(x, y, z) (((x) | ~(y)) ^ (z))
+#define IQ(x, y, z) (((x) & (z)) | ((y) & ~(z)))
+#define J(x, y, z) ((x) ^ ((y) | ~(z)))
+
+#define FF(a, b, c, d, e, x, s)        \
+    {                                  \
+        (a) += F((b), (c), (d)) + (x); \
+        (a) = ROL((a), (s)) + (e);     \
+        (c) = ROL((c), 10);            \
+    }
+#define GG(a, b, c, d, e, x, s)                       \
+    {                                                 \
+        (a) += G((b), (c), (d)) + (x) + 0x5a827999UL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+#define HH(a, b, c, d, e, x, s)                       \
+    {                                                 \
+        (a) += H((b), (c), (d)) + (x) + 0x6ed9eba1UL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+#define II(a, b, c, d, e, x, s)                        \
+    {                                                  \
+        (a) += IQ((b), (c), (d)) + (x) + 0x8f1bbcdcUL; \
+        (a) = ROL((a), (s)) + (e);                     \
+        (c) = ROL((c), 10);                            \
+    }
+#define JJ(a, b, c, d, e, x, s)                       \
+    {                                                 \
+        (a) += J((b), (c), (d)) + (x) + 0xa953fd4eUL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+#define FFF(a, b, c, d, e, x, s)       \
+    {                                  \
+        (a) += F((b), (c), (d)) + (x); \
+        (a) = ROL((a), (s)) + (e);     \
+        (c) = ROL((c), 10);            \
+    }
+#define GGG(a, b, c, d, e, x, s)                      \
+    {                                                 \
+        (a) += G((b), (c), (d)) + (x) + 0x7a6d76e9UL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+#define HHH(a, b, c, d, e, x, s)                      \
+    {                                                 \
+        (a) += H((b), (c), (d)) + (x) + 0x6d703ef3UL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+#define III(a, b, c, d, e, x, s)                       \
+    {                                                  \
+        (a) += IQ((b), (c), (d)) + (x) + 0x5c4dd124UL; \
+        (a) = ROL((a), (s)) + (e);                     \
+        (c) = ROL((c), 10);                            \
+    }
+#define JJJ(a, b, c, d, e, x, s)                      \
+    {                                                 \
+        (a) += J((b), (c), (d)) + (x) + 0x50a28be6UL; \
+        (a) = ROL((a), (s)) + (e);                    \
+        (c) = ROL((c), 10);                           \
+    }
+
+__device__ void compress(uint32_t* MDbuf, uint32_t* X)
+{
+    uint32_t aa = MDbuf[0], bb = MDbuf[1], cc = MDbuf[2], dd = MDbuf[3], ee = MDbuf[4];
+    uint32_t aaa = MDbuf[0], bbb = MDbuf[1], ccc = MDbuf[2], ddd = MDbuf[3], eee = MDbuf[4];
+
+    /* round 1 */
+    FF(aa, bb, cc, dd, ee, X[0], 11);
+    FF(ee, aa, bb, cc, dd, X[1], 14);
+    FF(dd, ee, aa, bb, cc, X[2], 15);
+    FF(cc, dd, ee, aa, bb, X[3], 12);
+    FF(bb, cc, dd, ee, aa, X[4], 5);
+    FF(aa, bb, cc, dd, ee, X[5], 8);
+    FF(ee, aa, bb, cc, dd, X[6], 7);
+    FF(dd, ee, aa, bb, cc, X[7], 9);
+    FF(cc, dd, ee, aa, bb, X[8], 11);
+    FF(bb, cc, dd, ee, aa, X[9], 13);
+    FF(aa, bb, cc, dd, ee, X[10], 14);
+    FF(ee, aa, bb, cc, dd, X[11], 15);
+    FF(dd, ee, aa, bb, cc, X[12], 6);
+    FF(cc, dd, ee, aa, bb, X[13], 7);
+    FF(bb, cc, dd, ee, aa, X[14], 9);
+    FF(aa, bb, cc, dd, ee, X[15], 8);
+
+    /* round 2 */
+    GG(ee, aa, bb, cc, dd, X[7], 7);
+    GG(dd, ee, aa, bb, cc, X[4], 6);
+    GG(cc, dd, ee, aa, bb, X[13], 8);
+    GG(bb, cc, dd, ee, aa, X[1], 13);
+    GG(aa, bb, cc, dd, ee, X[10], 11);
+    GG(ee, aa, bb, cc, dd, X[6], 9);
+    GG(dd, ee, aa, bb, cc, X[15], 7);
+    GG(cc, dd, ee, aa, bb, X[3], 15);
+    GG(bb, cc, dd, ee, aa, X[12], 7);
+    GG(aa, bb, cc, dd, ee, X[0], 12);
+    GG(ee, aa, bb, cc, dd, X[9], 15);
+    GG(dd, ee, aa, bb, cc, X[5], 9);
+    GG(cc, dd, ee, aa, bb, X[2], 11);
+    GG(bb, cc, dd, ee, aa, X[14], 7);
+    GG(aa, bb, cc, dd, ee, X[11], 13);
+    GG(ee, aa, bb, cc, dd, X[8], 12);
+
+    /* round 3 */
+    HH(dd, ee, aa, bb, cc, X[3], 11);
+    HH(cc, dd, ee, aa, bb, X[10], 13);
+    HH(bb, cc, dd, ee, aa, X[14], 6);
+    HH(aa, bb, cc, dd, ee, X[4], 7);
+    HH(ee, aa, bb, cc, dd, X[9], 14);
+    HH(dd, ee, aa, bb, cc, X[15], 9);
+    HH(cc, dd, ee, aa, bb, X[8], 13);
+    HH(bb, cc, dd, ee, aa, X[1], 15);
+    HH(aa, bb, cc, dd, ee, X[2], 14);
+    HH(ee, aa, bb, cc, dd, X[7], 8);
+    HH(dd, ee, aa, bb, cc, X[0], 13);
+    HH(cc, dd, ee, aa, bb, X[6], 6);
+    HH(bb, cc, dd, ee, aa, X[13], 5);
+    HH(aa, bb, cc, dd, ee, X[11], 12);
+    HH(ee, aa, bb, cc, dd, X[5], 7);
+    HH(dd, ee, aa, bb, cc, X[12], 5);
+
+    /* round 4 */
+    II(cc, dd, ee, aa, bb, X[1], 11);
+    II(bb, cc, dd, ee, aa, X[9], 12);
+    II(aa, bb, cc, dd, ee, X[11], 14);
+    II(ee, aa, bb, cc, dd, X[10], 15);
+    II(dd, ee, aa, bb, cc, X[0], 14);
+    II(cc, dd, ee, aa, bb, X[8], 15);
+    II(bb, cc, dd, ee, aa, X[12], 9);
+    II(aa, bb, cc, dd, ee, X[4], 8);
+    II(ee, aa, bb, cc, dd, X[13], 9);
+    II(dd, ee, aa, bb, cc, X[3], 14);
+    II(cc, dd, ee, aa, bb, X[7], 5);
+    II(bb, cc, dd, ee, aa, X[15], 6);
+    II(aa, bb, cc, dd, ee, X[14], 8);
+    II(ee, aa, bb, cc, dd, X[5], 6);
+    II(dd, ee, aa, bb, cc, X[6], 5);
+    II(cc, dd, ee, aa, bb, X[2], 12);
+
+    /* round 5 */
+    JJ(bb, cc, dd, ee, aa, X[4], 9);
+    JJ(aa, bb, cc, dd, ee, X[0], 15);
+    JJ(ee, aa, bb, cc, dd, X[5], 5);
+    JJ(dd, ee, aa, bb, cc, X[9], 11);
+    JJ(cc, dd, ee, aa, bb, X[7], 6);
+    JJ(bb, cc, dd, ee, aa, X[12], 8);
+    JJ(aa, bb, cc, dd, ee, X[2], 13);
+    JJ(ee, aa, bb, cc, dd, X[10], 12);
+    JJ(dd, ee, aa, bb, cc, X[14], 5);
+    JJ(cc, dd, ee, aa, bb, X[1], 12);
+    JJ(bb, cc, dd, ee, aa, X[3], 13);
+    JJ(aa, bb, cc, dd, ee, X[8], 14);
+    JJ(ee, aa, bb, cc, dd, X[11], 11);
+    JJ(dd, ee, aa, bb, cc, X[6], 8);
+    JJ(cc, dd, ee, aa, bb, X[15], 5);
+    JJ(bb, cc, dd, ee, aa, X[13], 6);
+
+    /* parallel round 1 */
+    JJJ(aaa, bbb, ccc, ddd, eee, X[5], 8);
+    JJJ(eee, aaa, bbb, ccc, ddd, X[14], 9);
+    JJJ(ddd, eee, aaa, bbb, ccc, X[7], 9);
+    JJJ(ccc, ddd, eee, aaa, bbb, X[0], 11);
+    JJJ(bbb, ccc, ddd, eee, aaa, X[9], 13);
+    JJJ(aaa, bbb, ccc, ddd, eee, X[2], 15);
+    JJJ(eee, aaa, bbb, ccc, ddd, X[11], 15);
+    JJJ(ddd, eee, aaa, bbb, ccc, X[4], 5);
+    JJJ(ccc, ddd, eee, aaa, bbb, X[13], 7);
+    JJJ(bbb, ccc, ddd, eee, aaa, X[6], 7);
+    JJJ(aaa, bbb, ccc, ddd, eee, X[15], 8);
+    JJJ(eee, aaa, bbb, ccc, ddd, X[8], 11);
+    JJJ(ddd, eee, aaa, bbb, ccc, X[1], 14);
+    JJJ(ccc, ddd, eee, aaa, bbb, X[10], 14);
+    JJJ(bbb, ccc, ddd, eee, aaa, X[3], 12);
+    JJJ(aaa, bbb, ccc, ddd, eee, X[12], 6);
+
+    /* parallel round 2 */
+    III(eee, aaa, bbb, ccc, ddd, X[6], 9);
+    III(ddd, eee, aaa, bbb, ccc, X[11], 13);
+    III(ccc, ddd, eee, aaa, bbb, X[3], 15);
+    III(bbb, ccc, ddd, eee, aaa, X[7], 7);
+    III(aaa, bbb, ccc, ddd, eee, X[0], 12);
+    III(eee, aaa, bbb, ccc, ddd, X[13], 8);
+    III(ddd, eee, aaa, bbb, ccc, X[5], 9);
+    III(ccc, ddd, eee, aaa, bbb, X[10], 11);
+    III(bbb, ccc, ddd, eee, aaa, X[14], 7);
+    III(aaa, bbb, ccc, ddd, eee, X[15], 7);
+    III(eee, aaa, bbb, ccc, ddd, X[8], 12);
+    III(ddd, eee, aaa, bbb, ccc, X[12], 7);
+    III(ccc, ddd, eee, aaa, bbb, X[4], 6);
+    III(bbb, ccc, ddd, eee, aaa, X[9], 15);
+    III(aaa, bbb, ccc, ddd, eee, X[1], 13);
+    III(eee, aaa, bbb, ccc, ddd, X[2], 11);
+
+    /* parallel round 3 */
+    HHH(ddd, eee, aaa, bbb, ccc, X[15], 9);
+    HHH(ccc, ddd, eee, aaa, bbb, X[5], 7);
+    HHH(bbb, ccc, ddd, eee, aaa, X[1], 15);
+    HHH(aaa, bbb, ccc, ddd, eee, X[3], 11);
+    HHH(eee, aaa, bbb, ccc, ddd, X[7], 8);
+    HHH(ddd, eee, aaa, bbb, ccc, X[14], 6);
+    HHH(ccc, ddd, eee, aaa, bbb, X[6], 6);
+    HHH(bbb, ccc, ddd, eee, aaa, X[9], 14);
+    HHH(aaa, bbb, ccc, ddd, eee, X[11], 12);
+    HHH(eee, aaa, bbb, ccc, ddd, X[8], 13);
+    HHH(ddd, eee, aaa, bbb, ccc, X[12], 5);
+    HHH(ccc, ddd, eee, aaa, bbb, X[2], 14);
+    HHH(bbb, ccc, ddd, eee, aaa, X[10], 13);
+    HHH(aaa, bbb, ccc, ddd, eee, X[0], 13);
+    HHH(eee, aaa, bbb, ccc, ddd, X[4], 7);
+    HHH(ddd, eee, aaa, bbb, ccc, X[13], 5);
+
+    /* parallel round 4 */
+    GGG(ccc, ddd, eee, aaa, bbb, X[8], 15);
+    GGG(bbb, ccc, ddd, eee, aaa, X[6], 5);
+    GGG(aaa, bbb, ccc, ddd, eee, X[4], 8);
+    GGG(eee, aaa, bbb, ccc, ddd, X[1], 11);
+    GGG(ddd, eee, aaa, bbb, ccc, X[3], 14);
+    GGG(ccc, ddd, eee, aaa, bbb, X[11], 14);
+    GGG(bbb, ccc, ddd, eee, aaa, X[15], 6);
+    GGG(aaa, bbb, ccc, ddd, eee, X[0], 14);
+    GGG(eee, aaa, bbb, ccc, ddd, X[5], 6);
+    GGG(ddd, eee, aaa, bbb, ccc, X[12], 9);
+    GGG(ccc, ddd, eee, aaa, bbb, X[2], 12);
+    GGG(bbb, ccc, ddd, eee, aaa, X[13], 9);
+    GGG(aaa, bbb, ccc, ddd, eee, X[9], 12);
+    GGG(eee, aaa, bbb, ccc, ddd, X[7], 5);
+    GGG(ddd, eee, aaa, bbb, ccc, X[10], 15);
+    GGG(ccc, ddd, eee, aaa, bbb, X[14], 8);
+
+    /* parallel round 5 */
+    FFF(bbb, ccc, ddd, eee, aaa, X[12], 8);
+    FFF(aaa, bbb, ccc, ddd, eee, X[15], 5);
+    FFF(eee, aaa, bbb, ccc, ddd, X[10], 12);
+    FFF(ddd, eee, aaa, bbb, ccc, X[4], 9);
+    FFF(ccc, ddd, eee, aaa, bbb, X[1], 12);
+    FFF(bbb, ccc, ddd, eee, aaa, X[5], 5);
+    FFF(aaa, bbb, ccc, ddd, eee, X[8], 14);
+    FFF(eee, aaa, bbb, ccc, ddd, X[7], 6);
+    FFF(ddd, eee, aaa, bbb, ccc, X[6], 8);
+    FFF(ccc, ddd, eee, aaa, bbb, X[2], 13);
+    FFF(bbb, ccc, ddd, eee, aaa, X[13], 6);
+    FFF(aaa, bbb, ccc, ddd, eee, X[14], 5);
+    FFF(eee, aaa, bbb, ccc, ddd, X[0], 15);
+    FFF(ddd, eee, aaa, bbb, ccc, X[3], 13);
+    FFF(ccc, ddd, eee, aaa, bbb, X[9], 11);
+    FFF(bbb, ccc, ddd, eee, aaa, X[11], 11);
+
+    /* combine results */
+    ddd += cc + MDbuf[1];
+    MDbuf[1] = MDbuf[2] + dd + eee;
+    MDbuf[2] = MDbuf[3] + ee + aaa;
+    MDbuf[3] = MDbuf[4] + aa + bbb;
+    MDbuf[4] = MDbuf[0] + bb + ccc;
+    MDbuf[0] = ddd;
 }
 
-// RIPEMD-160 non-linear functions
-__device__ __inline__ uint32_t F(uint32_t x, uint32_t y, uint32_t z) { return x ^ y ^ z; }
-__device__ __inline__ uint32_t G(uint32_t x, uint32_t y, uint32_t z) { return (x & y) | (~x & z); }
-__device__ __inline__ uint32_t H(uint32_t x, uint32_t y, uint32_t z) { return (x | ~y) ^ z; }
-__device__ __inline__ uint32_t I(uint32_t x, uint32_t y, uint32_t z) { return (x & z) | (y & ~z); }
-__device__ __inline__ uint32_t J(uint32_t x, uint32_t y, uint32_t z) { return x ^ (y | ~z); }
+__global__ void ripemd160_gpu(const uint8_t* msg, uint32_t msg_len, uint8_t* hash)
+{
+    uint32_t i;
+    int j;
+    uint32_t digest[5] = {0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0UL};
 
-// Message schedule index arrays for each round
-__device__ const uint32_t R[80] = {
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8,
-    3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12,
-    1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2,
-    4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13
-};
+    for (i = 0; i < (msg_len >> 6); ++i) {
+        uint32_t chunk[16];
 
-__device__ const uint32_t Rp[80] = {
-    5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12,
-    6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2,
-    15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13,
-    8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14,
-    12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11
-};
+        for (j = 0; j < 16; ++j) {
+            chunk[j] = (uint32_t)(*(msg++));
+            chunk[j] |= (uint32_t)(*(msg++)) << 8;
+            chunk[j] |= (uint32_t)(*(msg++)) << 16;
+            chunk[j] |= (uint32_t)(*(msg++)) << 24;
+        }
 
-// Shift amounts for each round
-__device__ const uint32_t S[80] = {
-    11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8,
-    7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12,
-    11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5,
-    11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12,
-    9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6
-};
-
-__device__ const uint32_t Sp[80] = {
-    8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6,
-    9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11,
-    9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5,
-    15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8,
-    8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11
-};
-
-// RIPEMD-160 transformation function
-__device__ void ripemd160_transform(uint32_t state[5], const unsigned char block[RIPEMD160_BLOCK_SIZE]) {
-    uint32_t a, b, c, d, e, ap, bp, cp, dp, ep, t;
-    uint32_t w[16];
-    int i;
-
-    // Initialize message schedule
-    for (i = 0; i < 16; i++) {
-        w[i] = (block[i * 4] | (block[i * 4 + 1] << 8) | (block[i * 4 + 2] << 16) | (block[i * 4 + 3] << 24));
+        compress(digest, chunk);
     }
 
-    // Initialize working variables
-    a = ap = state[0];
-    b = bp = state[1];
-    c = cp = state[2];
-    d = dp = state[3];
-    e = ep = state[4];
+    // Last chunk
+    {
+        uint32_t chunk[16] = {0};
 
-    // Perform main computation
-    for (i = 0; i < 80; i++) {
-        t = rol(a + F(b, c, d) + w[R[i]] + K[i / 16], S[i]) + e;
-        a = e;
-        e = d;
-        d = rol(c, 10);
-        c = b;
-        b = t;
+        for (i = 0; i < (msg_len & 63); ++i) {
+            chunk[i >> 2] ^= (uint32_t)*msg++ << ((i & 3) << 3);
+        }
 
-        t = rol(ap + J(bp, cp, dp) + w[Rp[i]] + Kp[i / 16], Sp[i]) + ep;
-        ap = ep;
-        ep = dp;
-        dp = rol(cp, 10);
-        cp = bp;
-        bp = t;
+        chunk[(msg_len >> 2) & 15] ^= (uint32_t)1 << (8 * (msg_len & 3) + 7);
+
+        if ((msg_len & 63) > 55) {
+            compress(digest, chunk);
+            memset(chunk, 0, 64);
+        }
+
+        chunk[14] = msg_len << 3;
+        chunk[15] = (msg_len >> 29);
+        compress(digest, chunk);
     }
 
-    t = state[1] + c + dp;
-    state[1] = state[2] + d + ep;
-    state[2] = state[3] + e + ap;
-    state[3] = state[4] + a + bp;
-    state[4] = state[0] + b + cp;
-    state[0] = t;
-}
-
-// RIPEMD-160 initialization
-__device__ void ripemd160_init(uint32_t state[5]) {
-    state[0] = 0x67452301;
-    state[1] = 0xefcdab89;
-    state[2] = 0x98badcfe;
-    state[3] = 0x10325476;
-    state[4] = 0xc3d2e1f0;
-}
-
-// RIPEMD-160 GPU implementation
-__device__ void ripemd160_gpu(const unsigned char* data, size_t len, unsigned char* hash) {
-    uint32_t state[5];
-    unsigned char block[RIPEMD160_BLOCK_SIZE];
-    size_t i, bit_len = len * 8;
-
-    ripemd160_init(state);
-
-    // Process each 512-bit block
-    while (len >= RIPEMD160_BLOCK_SIZE) {
-        memcpy(block, data, RIPEMD160_BLOCK_SIZE);
-        ripemd160_transform(state, block);
-        data += RIPEMD160_BLOCK_SIZE;
-        len -= RIPEMD160_BLOCK_SIZE;
-    }
-
-    // Padding
-    memcpy(block, data, len);
-    block[len] = 0x80;  // Append '1' bit
-    if (len < RIPEMD160_BLOCK_SIZE - 8) {
-        memset(block + len + 1, 0, RIPEMD160_BLOCK_SIZE - len - 9);
-    } else {
-        memset(block + len + 1, 0, RIPEMD160_BLOCK_SIZE - len - 1);
-        ripemd160_transform(state, block);
-        memset(block, 0, RIPEMD160_BLOCK_SIZE - 8);
-    }
-
-    // Append length in bits (big-endian)
-    for (i = 0; i < 8; ++i) {
-        block[RIPEMD160_BLOCK_SIZE - 1 - i] = bit_len >> (i * 8);
-    }
-    ripemd160_transform(state, block);
-
-    // Convert state to hash (little-endian)
     for (i = 0; i < 5; ++i) {
-        hash[i * 4] = state[i] & 0xff;
-        hash[i * 4 + 1] = (state[i] >> 8) & 0xff;
-        hash[i * 4 + 2] = (state[i] >> 16) & 0xff;
-        hash[i * 4 + 3] = (state[i] >> 24) & 0xff;
+        *(hash++) = digest[i];
+        *(hash++) = digest[i] >> 8;
+        *(hash++) = digest[i] >> 16;
+        *(hash++) = digest[i] >> 24;
     }
-}
-
-// Test kernel to compute RIPEMD-160 on GPU
-__global__ void ripemd160_test_kernel(const unsigned char* data, size_t len, unsigned char* hash) {
-    ripemd160_gpu(data, len, hash);
 }
 
 int main() {
-    const char* input = "Hello, CUDA RIPEMD-160!";
+    // Ajustado de uma entrada utf-8 para hex com entrada de 33 bytes (chave comprimida)
+    const char* input_sha256 = "e846c2c3a5c927c9d23d5aa249c838a91256174db96c61c9e5d340aaae9962e6";
+    unsigned char byte_array[32];
+
+    // Converta a string hex para bytes
+    for (int i = 0; i < 32; i++) {
+        sscanf(input_sha256 + 2*i, "%2hhx", &byte_array[i]);
+    }
+
     unsigned char hash[RIPEMD160_DIGEST_SIZE];
 
     // Allocate memory on GPU
     unsigned char* d_data;
     unsigned char* d_hash;
-    size_t input_len = strlen(input);
-
+    size_t input_len = sizeof(byte_array);
+    
     cudaMalloc((void**)&d_data, input_len);
     cudaMalloc((void**)&d_hash, RIPEMD160_DIGEST_SIZE);
 
     // Copy data to GPU
-    cudaMemcpy(d_data, input, input_len, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data, byte_array, input_len, cudaMemcpyHostToDevice);
 
-    // Launch kernel to compute RIPEMD-160
-    ripemd160_test_kernel<<<1, 1>>>(d_data, input_len, d_hash);
+    // Launch kernel to compute SHA-256
+    ripemd160_gpu<<<1, 1>>>(d_data, input_len, d_hash);
 
     // Copy hash result back to host
     cudaMemcpy(hash, d_hash, RIPEMD160_DIGEST_SIZE, cudaMemcpyDeviceToHost);
 
     // Print the result
-    printf("RIPEMD-160 hash: ");
+    printf("RIPEMD160 hash: ");
     for (int i = 0; i < RIPEMD160_DIGEST_SIZE; ++i) {
         printf("%02x", hash[i]);
     }
